dc9555413a7e9bb004ff6d8e53bf47f98163e368
syuaweu <105784560+syuaweu@users.noreply.github.com>
2024-05-24 11:07:08 +0800

    finish score bar

--- Source/Game/mygame.h
+++ Source/Game/mygame.h
@@ -120,12 +120,15 @@ namespace game_framework {
 		CMovingBitmap condition_icon[10];
 		CMovingBitmap game_over;
 		CMovingBitmap win;
+		CMovingBitmap score_image[250];
+		CMovingBitmap score_edge;
 		int all_condition_number = 0;
 		int score = 0;
 		int moves;
 		bool has_conditon_type[3];
+		int star_score[3];
 		vector<pair<int, int>> condition_number[3];
-		void LoadWinCondition(string map_name);
+		void LoadWinCondition();
 		void ScoreAndMovesCalculate(vector<vector<int>> s);
 		bool isGameOver();
 		bool isWin();
